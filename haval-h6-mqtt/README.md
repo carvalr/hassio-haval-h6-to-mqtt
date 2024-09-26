7722a89ffd3893d6e1a90fa953466af4975b4da0
Robson.Carvalho <robson.carvalho@sovos.com>
2024-09-26 02:32:06 -0300

    Initial commit

---
+++ haval-h6-mqtt/README.md
@@ -0,0 +1,135 @@
+# Home Assistant Community Add-on: GWM Brasil Haval H6 com MQTT
+
+## Sobre
+
+Este add-on permite a integração do Home Assistant com o veículo GWM **Brasil** Haval H6 utilizando MQTT. Com essa integração, é possível monitorar e controlar várias funcionalidades do veículo diretamente pelo Home Assistant.
+
+[![Add Add-on to Home Assistant](https://my.home-assistant.io/badges/supervisor_add_addon_repository.svg)](https://my.home-assistant.io/redirect/supervisor_add_addon_repository/?repository_url=https%3A%2F%2Fgithub.com%2Fhavaleiros%2Fhassio-haval-h6-to-mqtt)
+
+### Passo a Passo para adicionar o add-on ao Home Assistant
+
+#### 1. Adicionando o Repositório do Add-on
+
+1. Acesse a interface web do Home Assistant.
+2. Navegue até **Supervisor** no menu lateral.
+3. Selecione a aba **Add-on Store**.
+4. Clique nos três pontos no canto superior direito e selecione **Repositórios**.
+5. Adicione o URL do repositório do add-on: https://github.com/havaleiros/hassio-haval-h6-to-mqtt
+6. Clique em **Adicionar** e, em seguida, feche a janela de repositórios.
+
+#### 2. Instalando o Add-on
+
+1. Na aba **Add-on Store**, role para baixo até encontrar o repositório que você acabou de adicionar.
+2. Selecione o add-on **GWM Brasil Haval H6 com MQTT**.
+3. Clique em **Instalar** e aguarde a conclusão da instalação.
+
+#### 3. Configurando o Add-on
+
+1. Após a instalação, vá até a aba **Ajustes** do add-on.
+2. Configure os parâmetros necessários como endereço do broker MQTT, credenciais, e outras opções específicas para o Haval H6.
+```yaml
+haval_username: conta_de_email_vinculada_ao_app_MyGWM
+haval_password: senha_do_app_MyGWM
+haval_vin: CHASSIS_DO_CARRO
+haval_pin: senha_de_ativação_de_comandos_no_app_MyGWM
+mqtt_server: mqtt://homeassistant.local:1883
+mqtt_user: nome_de_usuario_do_mqtt
+mqtt_pass: senha_do_mqtt
+refresh_time: tempo_em_segundos (mínimo de 5 segundos)
+device_tracker_enabled: true [ou] false
+```
+
+![Ajustes dos add-on](https://raw.githubusercontent.com/havaleiros/hassio-haval-h6-to-mqtt/main/haval-h6-mqtt/images/addon_settings.png)
+
+Nota: Caso o campo `mqtt_pass` não esteja disponível na tela, ative a opção _Mostrar opções de configuração opcionais não utilizadas_.
+
+3. Salve as configurações.
+4. Iniciando o Add-on
+Volte para a aba Informação do add-on.
+Clique em Iniciar para inicializar o add-on.
+Habilite a opção Iniciar na Inicialização se desejar que o add-on inicie automaticamente junto com o Home Assistant.
+5. Verificando a Integração
+Vá para Configuração no menu lateral do Home Assistant.
+Selecione Ferramentas de desenvolvedor.
+Verifique se as entidades iniciadas com *sensor.haval_* estão listados na aba Estado.
+Agora, você deve ser capaz de monitorar o seu veículo diretamente pelo painel do Home Assistant.
+
+### Adicionando um Novo Dashboard no Home Assistant
+
+#### Passo a Passo
+
+1. Acesse a interface web do Home Assistant.
+2. Navegue até **Configurações** no menu lateral.
+3. Selecione **Dashboards**.
+4. Clique em **Adicionar Dashboard**.
+5. Atribua o título como `haval` e o ícone como `mdi:car-electric`. Ative a opção `Mostrar na barra lateral` e clique em _CRIAR_.
+6. Na nova linha criada com o novo dashboard, clique em _ABRIR_.
+7. No canto superior esquerdo da tela, clique em _Editar dashboard_.
+8. Clique novamente no símbolo com 3 pontos verticais e depois em _Editor de configuração RAW_.
+5. Apague o conteúdo existente que será exibido, copie o conteúdo do arquivo `HomeAssistant_Dashboard_Haval.yaml` fornecido como template e cole nesta tela. [Baixe aqui o arquivo YAML](https://github.com/havaleiros/hassio-haval-h6-to-mqtt/blob/main/haval-h6-mqtt/files/HomeAssistant_Dashboard_Haval.yaml).
+
+#### Adicionando Imagens do Veículo
+
+1. Baixe o arquivo [Baixe aqui o arquivo haval_h6.zip](https://github.com/havaleiros/hassio-haval-h6-to-mqtt/raw/main/haval-h6-mqtt/files/haval_h6.zip).
+2. Descompacte o arquivo e salve as imagens na pasta `www/images/haval_h6` do Home Assistant.
+
+O resultado esperado é que as imagens devem ficar no caminho `www/images/haval_h6/[imagem].png`
+Não salve o arquivo `haval_h6.zip` diretamente no diretório do Home Assistant, pois isto não terá efeito no uso das imagens. 
+
+#### Dica
+Recomenda-se o uso do Add-on [Samba share](https://github.com/home-assistant/addons/tree/master/samba) para acesso na pasta `www` de sua instância do Home Assistant.
+Desta forma não haverá necessidde de copiar imagem por imagem para a pasta.
+
+#### Instalando Custom Cards
+
+Para uma melhor experiência visual, é necessário instalar alguns custom cards através do HACS (Home Assistant Community Store). Siga o [guia de instalação do HACS](https://hacs.xyz/docs/setup/download) para configurá-lo no seu Home Assistant.
+
+Os seguintes custom cards são necessários:
+
+- [stack-in-card](https://github.com/custom-cards/stack-in-card)
+- [button-card](https://github.com/custom-cards/button-card)
+- [mushroom-title-card](https://github.com/piitaya/lovelace-mushroom)
+- [template-entity-row](https://github.com/thomasloven/lovelace-template-entity-row)
+- [card-mod](https://github.com/thomasloven/lovelace-card-mod)
+- [bar-card](https://github.com/custom-cards/bar-card)
+- [fold-entity-row](https://github.com/thomasloven/lovelace-fold-entity-row)
+
+Depois de instalar o HACS, siga as instruções específicas de cada card para adicioná-los ao seu dashboard.
+
+
+Agora, seu novo dashboard estará configurado para exibir informações detalhadas sobre o seu veículo GWM Haval H6.
+
+![Exemplo de painel no Home Assistant](https://raw.githubusercontent.com/havaleiros/hassio-haval-h6-to-mqtt/main/haval-h6-mqtt/images/HomeAssistant_Example.png)
+
+## Suporte
+Para dúvidas, problemas ou sugestões, abra uma issue.
+
+## Contato
+A comunidade Havaleiros está no WhatsApp. Você pode solicitar acesso enviando email para havaleiros@gmail.com ou gentefelizclube@gmail.com.
+
+![Havaleiros Brasil](https://raw.githubusercontent.com/havaleiros/hassio-haval-h6-to-mqtt/main/haval-h6-mqtt/images/Havaleiros_logo_Quadrado.png)
+
+## Contribuições
+Contribuições são bem-vindas! Sinta-se à vontade para abrir pull requests ou issues no repositório do GitHub.
+
+Obrigado por utilizar o add-on GWM Brasil Haval H6 com MQTT para Home Assistant. Aproveite a integração!
+
+## Créditos
+
+Este projeto foi possível devido ao trabalho executado em https://github.com/ipsBruno/haval-h6-gwm-alexa-chatgpt-mqtt-integration, que por sua vez utilizou o trabalho disponível em https://github.com/zivillian/ora2mqtt.
+
+Contribuições de: 
+- @paulovitin
+- @bobaoapae
+- @carvalr
+
+## Licença
+Licença MIT
+
+Copyright (c) 2024 Havaleiros
+
+É concedida permissão, gratuitamente, a qualquer pessoa que obtenha uma cópia deste software e arquivos de documentação associados (o "Software"), para lidar no Software sem restrições, incluindo, sem limitação, os direitos usar, copiar, modificar, mesclar, publicar, distribuir, sublicenciar e/ou vender cópias do Software e permitir que as pessoas a quem o Software é capacitado para fazê-lo, sujeito às seguintes condições:
+
+O aviso de direitos autorais acima e este aviso de permissão serão incluídos em todos cópias ou partes substanciais do Software.
+
+O SOFTWARE É FORNECIDO "COMO ESTÁ", SEM GARANTIA DE QUALQUER TIPO, EXPRESSA OU IMPLÍCITAS, INCLUINDO, MAS NÃO SE LIMITANDO ÀS GARANTIAS DE COMERCIALIZAÇÃO, ADEQUAÇÃO A UM DETERMINADO FIM E NÃO VIOLAÇÃO. EM HIPÓTESE ALGUMA O OS AUTORES OU DETENTORES DE DIREITOS AUTORAIS SERÃO RESPONSÁVEIS POR QUALQUER RECLAMAÇÃO, DANOS OU OUTROS RESPONSABILIDADE, SEJA EM UMA AÇÃO DE CONTRATO, ATO ILÍCITO OU DE OUTRA FORMA, DECORRENTE DE, FORA DE OU EM CONEXÃO COM O SOFTWARE OU O USO OU OUTRAS NEGOCIAÇÕES NO PROGRAMAS.
---
+++ haval-h6-mqtt/mqtt.js
@@ -0,0 +1,243 @@
+const axios = require("./axios");
+const mqtt = require("mqtt");
+const md5 = require("md5");
+const storage = require("./storage");
+var slugify = require("slugify");
+
+require("dotenv").config();
+
+const { MQTT_HOST, MQTT_PASS, MQTT_USER, VIN, PIN } = process.env;
+
+const EntityType = {
+  SENSOR: "sensor",
+  BINARY_SENSOR: "binary_sensor",
+  INPUT_TEXT: "input_text",
+  IMAGE: "image",
+  DEVICE_TRACKER: "device_tracker",
+  SWITCH: "switch",
+  BUTTON: "button",
+};
+
+let topicsAndActions = JSON.parse(storage.getItem('topicsAndActions')) || {};
+let topicsToSubscribe = JSON.parse(storage.getItem('topicsToSubscribe')) || {};
+
+const mqttModule = {
+  connect() {
+    return mqtt.connect(MQTT_HOST, {
+      password: MQTT_PASS,
+      username: MQTT_USER,
+    });
+  },
+  checkConnection() {
+    return new Promise((resolve, reject) => {
+      const client = mqttModule.connect();
+      client.on("connect", resolve);
+      client.on("error", reject);
+      client.end();
+    });
+  },
+  sendMqtt(topic, payload, options){
+    const client = mqttModule.connect();
+
+    client.on("connect", () => {
+      client.publish(topic, payload, options, (err) => {
+          if (err) console.error(err);
+          client.end();
+      });
+    });
+  },
+  remove(entityType, code) {
+    var payload = "";
+    var topic = `homeassistant/${entityType.toLowerCase()}/haval_${VIN.toLowerCase()}_${code}/config`;
+    mqttModule.sendMqtt(topic, JSON.stringify(payload), { retain: true });
+
+    var legacyTopic = `homeassistant/sensor/haval_${VIN.toLowerCase()}_${code}/config`;
+    mqttModule.sendMqtt(legacyTopic, JSON.stringify(payload), { retain: true });
+  },  
+  register(entityType, code, name, unit = null, device_class = "None", icon = null, actionable) {
+    
+    mqttModule.remove(entityType, code);
+    mqttModule.remove(entityType, "None");
+    
+    const slugName = slugify(name.toLowerCase(), "_");
+    var topic = `homeassistant/${entityType.toLowerCase()}/haval_${VIN.toLowerCase()}_${code}/config`;
+
+    let payload = {
+      unique_id: `haval_${VIN.toLowerCase()}_${slugName}`,
+      object_id: `haval_${VIN.toLowerCase()}_${slugName}`,
+      name      
+    };
+
+    if(entityType === EntityType.IMAGE){
+      payload.url_topic = `haval_${VIN.toLowerCase()}/${code}/state`;
+    }
+    if ([EntityType.SENSOR, EntityType.BINARY_SENSOR].includes(entityType)) {
+      if(device_class !== "None") payload.device_class = device_class;
+      payload.state_topic = `haval_${VIN.toLowerCase()}/${code}/state`;
+
+      if(entityType === EntityType.BINARY_SENSOR){
+        payload.payload_on = "1";
+        payload.payload_off = "0";
+      }
+    }
+    
+    if ([EntityType.SENSOR, EntityType.BINARY_SENSOR, EntityType.SWITCH, EntityType.BUTTON].includes(entityType) && icon) {
+      payload.icon = icon;
+    }
+
+    if (entityType === EntityType.SENSOR && unit !== null && !["-", " ", "_", "", "None", "null"].includes(unit)) {
+      payload.unit_of_measurement = unit;
+    }
+
+    if (entityType === EntityType.DEVICE_TRACKER) {
+      topic = `homeassistant/device_tracker/haval_${VIN.toLowerCase()}/config`;
+      payload.name = `haval_${VIN.toLowerCase()}`;
+      payload.unique_id = `haval_${VIN.toLowerCase()}`;
+      payload.object_id = `haval_${VIN.toLowerCase()}`;
+      payload.json_attributes_topic = `homeassistant/device_tracker/haval_${VIN.toLowerCase()}/attributes`;
+    }
+
+    if (entityType === EntityType.SWITCH) {      
+      topic = `homeassistant/switch/haval_${VIN.toLowerCase()}_${code.toLowerCase()}/config`;
+      payload.command_topic = `homeassistant/switch/haval_${VIN.toLowerCase()}_${code.toLowerCase()}/set`;
+      payload.optimistic = 'true';
+      payload.payload_on = 'ON';
+      payload.payload_off = 'OFF';
+    }
+
+    if (entityType === EntityType.BUTTON) {      
+      topic = `homeassistant/button/haval_${VIN.toLowerCase()}_${code.toLowerCase()}/config`;
+      payload.command_topic = `homeassistant/button/haval_${VIN.toLowerCase()}_${code.toLowerCase()}/press`;
+      payload.payload_press = 'PRESS';
+    }
+
+    mqttModule.sendMqtt(topic, JSON.stringify(payload), { retain: true });
+
+    if(actionable && PIN){
+      if(actionable.entity_type){
+        let topicToMonitorParent = payload.state_topic ? payload.state_topic : payload.command_topic;
+        topicsToSubscribe[`${entityType}_${VIN.toLowerCase()}_${code}`] = { topic: topicToMonitorParent };
+        topicsAndActions[`${actionable.entity_type}_${VIN.toLowerCase()}_${code}_${actionable.action.toLowerCase()}`] = { 
+          action: actionable.action, 
+          topic_to_monitor_parent: topicToMonitorParent, 
+          topic_to_monitor_actionable: "", 
+          topic_to_update: "", 
+          link_type: actionable.link_type };
+
+        mqttModule.register(entityType = EntityType[actionable.entity_type.toUpperCase()], 
+                            code = `${code}_${actionable.action.toLowerCase()}`,
+                            name = actionable.description,
+                            unit = null,
+                            device_class = "None",
+                            icon = actionable.icon,
+                            actionable = "Y");
+      }
+      else if (String(actionable) === "Y"){
+        if(topicsAndActions[`${entityType}_${VIN.toLowerCase()}_${code}`] && payload.command_topic){
+          topicsToSubscribe[`${entityType}_${VIN.toLowerCase()}_${code}`] = { topic: payload.command_topic };
+          topicsAndActions[`${entityType}_${VIN.toLowerCase()}_${code}`].topic_to_monitor_actionable = payload.command_topic;
+          topicsAndActions[`${entityType}_${VIN.toLowerCase()}_${code}`].topic_to_update = payload.command_topic;
+        }
+      }
+    }
+
+    storage.setItem('topicsAndActions', JSON.stringify(topicsAndActions));
+    storage.setItem('topicsToSubscribe', JSON.stringify(topicsToSubscribe));
+  },
+  sendDeviceTrackerUpdate(latitude, longitude, attributes) {
+    const json_attributes_topic = `homeassistant/device_tracker/haval_${VIN.toLowerCase()}/attributes`;
+    const gpsData = {
+      longitude: Number(longitude),
+      latitude: Number(latitude),
+      gps_accuracy: 60
+    }
+
+    const attributesPayload = Object.assign({}, gpsData, attributes);
+
+    mqttModule.sendMqtt(json_attributes_topic, JSON.stringify(attributesPayload), { retain: true });
+  },
+  sendMessage(code, value) {
+    const topic = `haval_${VIN.toLowerCase()}/${code}/state`;
+
+    mqttModule.sendMqtt(topic, String(value), { retain: true });
+  },
+};
+
+const ActionableAndLink = {
+  execute() {
+    const client = mqttModule.connect();
+
+    client.on('connect', () => {
+        Object.keys(topicsToSubscribe).forEach(function(key) {
+
+          client.subscribe(String(topicsToSubscribe[key].topic), (err) => {
+            if (err) {
+              console.error(`Error subscribing to topic [${String(topicsToSubscribe[key].topic)}]: `, err);
+            }
+          });
+        });
+
+        client.on('message', async (topic, message) => {  
+          let messageValue = message.toString().toUpperCase();
+
+          Object.keys(topicsAndActions).forEach(async function(key) {
+            if (topic === String(topicsAndActions[key].topic_to_monitor_actionable)){
+              if(storage.getItem('Startup') == "true") return;
+
+              if(String(topicsAndActions[key].action) === "airConditioner" && ["ON", "PRESS", "TRUE"].includes(messageValue)){
+                let seqNo = require('crypto').randomUUID().replaceAll('-', '') + '1234';
+                try {
+                  let acData = await axios.sendCmd({
+                                        "0x04": {
+                                          "airConditioner": {
+                                            "operationTime": "15",
+                                            "switchOrder": "1",
+                                            "temperature": "18"
+                                          }
+                                        }
+                                      },
+                                      0,
+                                      md5(PIN),
+                                      seqNo,
+                                      2,
+                                      VIN
+                                    );
+                  console.info("Command airConditioner executed: ", acData);
+                } catch(e){
+                  console.error(`***Error executing action [${String(topicsAndActions[key].action)}]***`);
+                  console.error(e);
+                }
+              }
+            }
+            else if (topic === String(topicsAndActions[key].topic_to_monitor_parent) && topicsAndActions[key].link_type && ["sync", "toggle"].includes(String(topicsAndActions[key].link_type))){
+              try {
+                if(messageValue === "0" || messageValue === "OFF" || messageValue === "FALSE"){
+                  
+                  mqttModule.sendMqtt(String(topicsAndActions[key].topic_to_update), 
+                                      String(topicsAndActions[key].link_type) === "sync" ? 'OFF' : String(topicsAndActions[key].link_type) === "toggle" ? 'ON' : 'OFF',
+                                      { retain: true });
+                }
+                else{
+                  mqttModule.sendMqtt(String(topicsAndActions[key].topic_to_update), 
+                                      String(topicsAndActions[key].link_type) === "sync" ? 'ON' : String(topicsAndActions[key].link_type) === "toggle" ? 'OFF' : 'ON', 
+                                      { retain: true });              
+                }
+              }
+              catch(e){
+                console.error(`***Error executing the parent and child status sync/toggle.***`)
+                console.error(`Parent topic: `, String(topicsAndActions[key].topic_to_monitor_parent));
+                console.error(`Child topic: `, String(topicsAndActions[key].topic_to_update));
+                console.error(e);
+              }
+            }
+          });
+        });
+    });
+
+    client.on('error', (err) => {
+      console.error('MQTT [ActionableAndLink] connection error:', err);
+    });
+  }
+}
+
+module.exports = { mqttModule, EntityType, ActionableAndLink };
