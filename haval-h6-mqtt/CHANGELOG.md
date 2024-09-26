047627cc5167b4245ee5ed15a9502cecaf7010c1
Robson.Carvalho <robson.carvalho@sovos.com>
2024-09-26 03:22:10 -0300

    Release 0.0.12

---
+++ haval-h6-mqtt/CHANGELOG.md
@@ -0,0 +1,40 @@
+# Changelog
+
+## 0.0.12
+- Atualizado o arquivo README para adicionar custom cards.
+- Adicionada opção de acionar o ar condicionado por 15 minutos, em 18 graus.
+- Modificado o painel lovelace para acionar opção de reduzir a visualização das entidades nos cards.
+- Remoção do log por clico para evitar overflow.
+- Parâmetro de configuração alterado de [refresh_minutes] para [refresh_time], agora com configuração em segundos ao invés de minutos. Mínimo de 5 segundos.
+- Adicionado botão para acionamento temporário do ar condicionado para ser utilizado em relógios inteligentes. *Veja o README para mais informações*.
+
+## 0.0.11
+- Ajuste nos códigos das portas que estavam invertidos.
+- Remove as entidades antes de registrá-las para garantir que o tópico de estado estará correto.
+- Tradução do arquivo changelog.
+
+## 0.0.10
+- Substituição dos sensores por binary_sensors para portas e renomeação deles.
+- *Alerta*: Renomear sensores fará com que alguns sensores anteriores fiquem indisponíveis, e quaisquer dashboards personalizados devem ser atualizados.
+- Novo Dashboard para Home Assistant. *Veja o README para mais informações*.
+- Adição de ícones para sensores sem device_class e binary_sensors.
+- Entidades são criadas apenas se o valor relacionado existir na resposta da API.
+- Adição de uma opção de configuração para desativar o device_tracker.
+- Removidas as configurações para unidade de pressão e caminho de dados.
+
+## 0.0.9
+- Correção de bug: Corrigir valor numérico não tratado da API ao criar atributos do device tracker.
+- Correções no arquivo map.js
+
+## 0.0.8
+- Atributos relacionados a ar-condicionado, portas e posição das janelas movidos de atributos do device tracker para sensores independentes.
+- README atualizado para adicionar a informação sobre a configuração oculta de senha MQTT.
+
+## 0.0.7
+- Adicionada entidade device_tracker e atributos.
+- Modificado o arquivo map.js para segregar sensores, atributos e itens não utilizados.
+- Adicionados os métodos [sendMqtt], [registerDeviceTracker] e [sendDeviceTrackerUpdate] ao mqtt.js.
+- Atualizado o index.js para atualizar informações relacionadas ao device_tracker e seus atributos.
+
+## 0.0.6
+- Versão inicial consolidada por @paulovitin
---
+++ haval-h6-mqtt/index.js
@@ -0,0 +1,235 @@
+const md5 = require("md5");
+const axios = require("./axios");
+const fs = require("fs");
+
+const storage = require("./storage");
+const { sensorTopics, attributeTopics } = require("./map");
+const { mqttModule, EntityType, ActionableAndLink } = require('./mqtt');
+const { checkConnection, register, sendDeviceTrackerUpdate, sendMessage } = mqttModule;
+
+const validationSchema = require('./schema')
+const { isTokenExpired } = require('./utils')
+
+require("dotenv").config();
+
+const { USERNAME, PASSWORD, REFRESH_TIME, DEVICE_TRACKER_ENABLED } = process.env;
+
+if (REFRESH_TIME < 5) {
+  console.error("The param refresh_time cannot be lower than 5.")
+  return;
+}
+
+const deviceid = storage.getItem("deviceid")
+  ? storage.getItem("deviceid")
+  : md5(Math.random().toString());
+
+storage.setItem("deviceid", deviceid);
+
+const userHeaders = {
+  appid: "6",
+  brand: "6",
+  brandid: "CCZ001",
+  country: "BR",
+  devicetype: "0",
+  enterpriseid: "CC01",
+  gwid: "",
+  language: "pt_BR",
+  rs: "5",
+  terminal: "GW_PC_GWM",
+};
+
+const auth = async () => {
+  const params = {
+    deviceid,
+    password: md5(PASSWORD),
+    account: USERNAME,
+  };
+
+  const accessToken = storage.getItem("accessToken")
+
+  if (accessToken && !isTokenExpired(accessToken)) {
+    return;
+  }
+
+  try {
+    const { data } = await axios.post(
+      "https://br-front-service.gwmcloud.com/br-official-commerce/br-official-gateway/pc-api/api/v1.0/userAuth/loginAccount",
+      params,
+      {
+        headers: userHeaders,
+      }
+    );
+
+    if (data.description === "SUCCESS") {
+      Object.keys(data.data).forEach((key) => {
+        storage.setItem(key, data.data[key]);
+      });
+
+      return data;
+    }
+
+    throw data;
+  } catch (err) {
+    console.error("Authentication error", err);
+    process.exit(0);
+  }
+};
+
+const getCarInfo = async () => {
+  try {
+    await auth();
+    const { data } = await axios.getCarInfo("vehicle/getLastStatus");
+    return data.data;
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+const getCarData = async () => {
+  try {
+    await auth();
+    const { data } = await axios.getCarInfo('vehicle/acquireVehicles');
+    return data.data[0];
+  } catch(e) {
+    console.error(e);
+  }
+}
+
+storage.setItem('Startup', "true");
+
+console.info("Flight check:");
+if (!fs.existsSync("./certs/gwm_general.cer"))
+  console.info("¡¡¡GWM general certicate not found!!!");
+
+if (!fs.existsSync("./certs/gwm_general.key"))
+  console.info("¡¡¡GWM general certificate key not found!!!");
+
+if (!fs.existsSync("./certs/gwm_root.cer"))
+  console.info("¡¡¡GWM root certificate not found!!!");
+
+validationSchema.validate(process.env)
+  .then(() => {
+    console.info('Check MQTT parameters')
+    return checkConnection();
+  })
+  .then(() => {
+    console.info('Retrieving car info')
+    return getCarInfo();
+  })
+  .then((data) => {
+    console.info("Registering entities");
+    data.items.forEach(({ code, value }) => {
+      if (sensorTopics.hasOwnProperty(code)) {
+        var { description, unit, device_class, entity_type, icon, actionable } = sensorTopics[code];
+
+        register(entityType = EntityType[entity_type.toUpperCase()], code = code, name = description, unit = unit, device_class = device_class, icon = icon, actionable = actionable);
+        sendMessage(code, value);
+      }
+    });
+    console.info("Activating actionables and linked entities");
+    ActionableAndLink.execute();
+  })
+  .then(() => {
+    console.info("Retrieving car data");
+    return getCarData();
+  })
+  .then((data) => {
+    console.info("Registering static entities");
+    const staticEntities = {
+      image: {
+        description: "Imagem do veículo",
+        entity_type: EntityType.SENSOR,
+        value: `${data.staticImageUrl}`,
+        icon: "mdi:image",
+      },
+      model: {
+        description: "Modelo do veículo",
+        entity_type: EntityType.SENSOR,
+        value: `${data.appShowSeriesName} ${data.powerType}`,
+        icon: "mdi:car-estate",
+      },
+      color: {
+        description: "Cor do veículo",
+        entity_type: EntityType.SENSOR,
+        value: `${data.color}`,
+        icon: "mdi:palette",
+      },
+      tankCapacity: {
+        description: "Capacidade do tanque",
+        entity_type: EntityType.SENSOR,
+        value: `${String(data.tankCapacity)}`,
+        icon: "mdi:gas-station",
+      },
+    }
+
+    Object.keys(staticEntities).forEach((code) => {
+      var { description, entity_type, value, icon } = staticEntities[code];
+
+      register(entityType = entity_type, code = code, name = description, unit = null, device_class = null, icon = icon, actionable = null);
+        
+      sendMessage(code, value);
+    });
+
+    const isDeviceTrackerEnabled = Boolean(DEVICE_TRACKER_ENABLED === 'true');
+    if(isDeviceTrackerEnabled){
+      storage.setItem('simIccid', data.simIccid);
+      storage.setItem('imsi', data.imsi);
+
+      var desc = `${data.appShowSeriesName} ${data.powerType} - ${data.color}`;
+      register(EntityType.DEVICE_TRACKER, code = "DeviceTracker", name = desc);
+      console.info('Device tracker registered')
+    }
+  })
+  .then(() => {
+    console.info('***STARTUP PROCESS FINISHED***')
+    storage.setItem('Startup', "false");
+  })
+  .catch((e) => {
+    console.error(e);
+    process.exit(0);
+  });
+
+const updateState = () => getCarInfo()
+  .then((data) => {
+    const attributes = {};
+    var slugify = require("slugify");
+
+    function getCurrentDateTime() {
+        const now = new Date();
+        const year = now.getFullYear();
+        const month = String(now.getMonth() + 1).padStart(2, '0'); // Mês começa do zero
+        const day = String(now.getDate()).padStart(2, '0');
+        const hours = String(now.getHours()).padStart(2, '0');
+        const minutes = String(now.getMinutes()).padStart(2, '0');
+        const seconds = String(now.getSeconds()).padStart(2, '0');
+
+        return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+    }
+    
+    try{
+        data.items.forEach(({ code, value }) => {
+          var sensorValue = value;
+          if (sensorTopics.hasOwnProperty(code)) {
+            sendMessage(code, sensorValue);
+          }
+          
+          if (attributeTopics.hasOwnProperty(code)) {
+            attributes[slugify(attributeTopics[code].description.toLowerCase(), "_")] = String(value);
+          }
+        });
+
+        const isDeviceTrackerEnabled = Boolean(DEVICE_TRACKER_ENABLED === 'true');
+        if(isDeviceTrackerEnabled){
+          attributes['simIccid'] = storage.getItem('simIccid');
+          attributes['imsi'] = storage.getItem('imsi');
+          attributes['icon'] = "mdi:car-electric-outline";
+
+          sendDeviceTrackerUpdate(String(data.latitude), String(data.longitude), attributes);
+        }
+    } catch (e) {
+      console.error(e);
+      process.exit(0);
+    }
+  });
+
+setInterval(async () => updateState(), (REFRESH_TIME || 1) * 1000);
